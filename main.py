3a00c8cb734e435aa77a4e1b751d91bdbb7132d3
Hardik Sonetta <45655747+hsonetta@users.noreply.github.com>
2021-06-07 16:23:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils import *
 from PyTorch_Object_Detection import detect
 from PyTorch_Object_Tracking import track
 
-videopath = r'C:/Users/hsone/Desktop/Extras/Projects/Object Tracking/data/video/dash_cam_2.mp4'
+videopath = r'Object Tracking/data/video/dash_cam_2.mp4'
 config_path='config/yolov3.cfg'
 weights_path='config/yolov3.weights'
 class_path='config/coco.names'
